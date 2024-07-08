a18f8450c4bc5bd8763d01d770ebdbfee09328e9
khushvant kumar <98275946+khushvantkr12@users.noreply.github.com>
2024-07-09 03:47:05 +0530

    Update ResetPassword.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -33,7 +33,7 @@ exports.resetPasswordToken = async (req, res) => {
         //link generation...
         //create url
         // const url = `http://localhost:3000/update-password/${token}`;
-        const url = `http://localhost:3000/update-password/${token}`;
+        const url = `https://e-shiksha-seven.vercel.app/update-password/${token}`;
 
         //sending... mail
         await mailSender(email, 
